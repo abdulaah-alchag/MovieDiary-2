228a1b0c54bb43866bb36447e23fdbdd45543a51
Souher Al-Taleb <altalebsouher8@gmail.com>
2025-11-21 11:07:51 +0100

    prettier-plugin-install, notes.js, ui.js erstellt

--- src/modules/getRefs.js
+++ src/modules/getRefs.js
@@ -1,7 +1,10 @@
-export const getRefs =()=>{
+export const getRefs = () => {
   return {
     movieContainer: document.querySelector(`#movies-container`),
-    favoriteControlCardButtons : document.querySelectorAll(".add-fav"),
-    detailsSection: document.querySelector("#movie-details"),
+    favoriteControlCardButtons: document.querySelectorAll(".add-fav"),
+    // detailsSection: document.querySelector("#movie-details"),
+
+    movieDetailsOverlay: document.querySelector("#movie-details-overlay"),
+    movieDetailsCard: document.querySelector("#movie-details-card"),
   };
 };
